4c689b127df894d00d88121a413c192bf49061a9
Alex Lawrence <144865444+Renzo-07@users.noreply.github.com>
2026-06-10 19:03:11 +0100

    Add personal information display in newjourney.c

--- newjourney.c
+++ newjourney.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello\n");
+  char name[ ] = "Alex";
+  int age = 18;
+  float height = 6.0;
+  printf("My name is %s\n", name);
+  printf("I am %d years old\n", age);
+  printf("I am %.1f feet tall\n", height);
 return 0;
 }
