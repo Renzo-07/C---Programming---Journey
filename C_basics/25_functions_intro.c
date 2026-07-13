53ebdc86885c501378fe44a08aa0148de01203b6
Alex Lawrence <144865444+Renzo-07@users.noreply.github.com>
2026-07-13 14:01:25 +0100

    Add greeting function and main function calls

---
+++ C_basics/25_functions_intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void greeting() {
+    printf("Hello! ");
+    printf("I am an example function. \n");
+}
+
+int main() {
+    greeting();
+    greeting();
+    greeting();
+    return 0;
+}
