d1b65257a782c9d0e5a922c51b2eb9f6a42e06ed
Alex Lawrence <144865444+Renzo-07@users.noreply.github.com>
2026-07-17 18:01:19 +0100

    Add square function with user input

---
+++ C_basics/27_square_function_userInput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int square(int number) {
+  return number * number;
+}
+
+int main() {
+int number;
+printf("Enter a number: ");
+scanf("%d", &number);
+printf("The square of %d is: %d\n", number, square(number));
+return 0;
+}
